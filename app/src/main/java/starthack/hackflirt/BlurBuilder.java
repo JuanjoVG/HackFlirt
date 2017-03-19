bc0013309c987fa26fe6264d17686b2a8026f747
Unknown <sergio.paredes25@gmail.com>
2017-03-19 03:51:51 +0100

    Add image field

---
+++ app/src/main/java/starthack/hackflirt/BlurBuilder.java
@@ -0,0 +1,74 @@
+package starthack.hackflirt;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+
+import java.io.IOException;
+
+public class BlurBuilder {
+    private static final float BITMAP_SCALE = 0.4f;
+    private static final float BLUR_RADIUS = 7.5f;
+
+    public static Bitmap blur(Context context, Bitmap image) {
+        int width = Math.round(image.getWidth() * BITMAP_SCALE);
+        int height = Math.round(image.getHeight() * BITMAP_SCALE);
+
+        Bitmap inputBitmap = Bitmap.createScaledBitmap(image, width, height, false);
+        Bitmap outputBitmap = Bitmap.createBitmap(inputBitmap);
+
+        RenderScript rs = RenderScript.create(context);
+        ScriptIntrinsicBlur theIntrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        Allocation tmpIn = Allocation.createFromBitmap(rs, inputBitmap);
+        Allocation tmpOut = Allocation.createFromBitmap(rs, outputBitmap);
+        theIntrinsic.setRadius(BLUR_RADIUS);
+        theIntrinsic.setInput(tmpIn);
+        theIntrinsic.forEach(tmpOut);
+        tmpOut.copyTo(outputBitmap);
+
+        return outputBitmap;
+    }
+
+    public static Bitmap modifyOrientation(Bitmap bitmap, String image_absolute_path) throws IOException {
+        ExifInterface ei = new ExifInterface(image_absolute_path);
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotate(bitmap, 90);
+
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotate(bitmap, 180);
+
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotate(bitmap, 270);
+
+            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+                return flip(bitmap, true, false);
+
+            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+                return flip(bitmap, false, true);
+
+            default:
+                return bitmap;
+        }
+    }
+
+    public static Bitmap rotate(Bitmap bitmap, float degrees) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degrees);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    public static Bitmap flip(Bitmap bitmap, boolean horizontal, boolean vertical) {
+        Matrix matrix = new Matrix();
+        matrix.preScale(horizontal ? -1 : 1, vertical ? -1 : 1);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+}
+
--- app/src/main/java/starthack/hackflirt/FillDetailsActivity.java
+++ app/src/main/java/starthack/hackflirt/FillDetailsActivity.java
@@ -1,24 +1,34 @@
 package starthack.hackflirt;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class FillDetailsActivity extends AppCompatActivity {
 
+    private static final int RESULT_LOAD_IMG = 1;
     private String gender;
     private String preference;
     private EditText city;
     private DatabaseReference mDatabase;
     private EditText age;
     private User user;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,43 @@ public class FillDetailsActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
         age = (EditText) findViewById(R.id.age);
         city = (EditText) findViewById(R.id.city);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getImageFromAlbum();
+            }
+        });
+    }
+
+    private void getImageFromAlbum() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+//                Bitmap orientedImage = BlurBuilder.modifyOrientation(selectedImage, imageUri.getPath());
+                Bitmap blur = BlurBuilder.blur(this, selectedImage);
+                imageView.setImageBitmap(blur);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Toast.makeText(this, "You haven't picked any image", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void onRadioButtonClicked(View view) {
@@ -66,20 +113,16 @@ public class FillDetailsActivity extends AppCompatActivity {
         if (gender == null) {
             Toast toast = Toast.makeText(getApplicationContext(), "Gender was not introduced", Toast.LENGTH_SHORT);
             toast.show();
-        }
-        else if (preference == null) {
+        } else if (preference == null) {
             Toast toast = Toast.makeText(getApplicationContext(), "Preferences was not introduced", Toast.LENGTH_SHORT);
             toast.show();
-        }
-        else if (ageString.trim().equals("")) {
+        } else if (ageString.trim().equals("")) {
             Toast toast = Toast.makeText(getApplicationContext(), "Age was not introduced", Toast.LENGTH_SHORT);
             toast.show();
-        }
-        else if (cityString.trim().equals("")) {
+        } else if (cityString.trim().equals("")) {
             Toast toast = Toast.makeText(getApplicationContext(), "City was not introduced", Toast.LENGTH_SHORT);
             toast.show();
-        }
-        else {
+        } else {
             mDatabase.child("user").child(user.getUid()).child("gender").setValue(gender);
             mDatabase.child("user").child(user.getUid()).child("preference").setValue(preference);
             mDatabase.child("user").child(user.getUid()).child("age").setValue(ageString);
